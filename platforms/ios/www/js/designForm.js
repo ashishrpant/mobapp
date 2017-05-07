772c16f77c1d8e00598970b830c459f893f318e6
Ashish Pant <ashish@arpant.com>
2017-05-07 09:49:45 -0400

    All files

---
+++ platforms/ios/www/js/designForm.js
@@ -0,0 +1,11 @@
+/**
+ * Created by ash on 4/11/17.
+ */
+
+class DesignForm {
+
+    LoadFormListSubmitDiam(diameterType){
+        console.log(diameterType);
+    }
+
+}
---
+++ platforms/ios/www/js/orderwithquote.js
@@ -0,0 +1,12 @@
+/**
+ * Created by ash on 5/1/17.
+ */
+
+class OrderWithQuote {
+
+    SearchQuoteNo(){
+        console.log("Ordering with quote");
+    }
+
+
+}
