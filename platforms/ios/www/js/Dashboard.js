b41e1dbd4ebc57cddd95715acd78f6e2ec71bf2f
Ashish Pant <ashish@arpant.com>
2017-06-08 09:42:10 -0400

    Uploading new JS files for dashboard

---
+++ platforms/ios/www/js/Dashboard.js
@@ -0,0 +1,48 @@
+/**
+ * Created by ash on 6/7/17.
+ */
+
+class Dashboard {
+
+  GoToDashBoard(loginName){
+
+    console.log(loginName);
+    $.ajax({
+      type          : "POST",
+      url           : localurl+'getDashBoard.html',
+      contentType   : "application/x-www-form-urlencoded;"
+    }).done(function(response) {
+      $("#inner").show();
+      $("#home").hide();
+      $('#navbar').collapse('hide');
+      $('#nav_child').show();
+
+
+      var message = response;
+      message     = message.replace("LOGINNAME",loginName);
+      //var newmessage = $(message).find('#loginName').html(loginName);
+
+      $("#load-container").html(message);
+
+    });
+  }
+
+  EditProfile(){
+
+  }
+
+  EditProfileAction(){
+
+  }
+
+  ChangePassword(){
+
+  }
+
+  ChangePasswordAction(){
+
+  }
+
+  
+
+}
